e1726b40d02b25105669f2a080b074d649dc364f
yami5 <yami5@wp.pl>
2017-07-28 10:57:49 +0200

    remove redundant code

--- FilesBrowser.py
+++ FilesBrowser.py
@@ -1,93 +1,65 @@
 import os
 import curses
-def ShowWhatDirectoryContains():
-        directorylist=os.listdir()
-        pad = curses.newpad(100, 50)
+def ShowWhatDirectoryContains(DirectoryList,ItemNumber):
+        DirectoryList=os.listdir()
+        pad = curses.newpad(20, 50)
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
         if len(os.getcwd())>3: 
-               for x in range(0,len(directorylist)+1):
+               for x in range(0,len(DirectoryList)+1):
                        if x>0:
                                if x==ItemNumber:
-                                       pad.addstr(x,0,directorylist[x-1],curses.color_pair(1))
-                               else: pad.addstr(x,0,directorylist[x-1])
+                                       pad.addstr(x,0,DirectoryList[x-1],curses.color_pair(1))
+                               else: pad.addstr(x,0,DirectoryList[x-1])
                        else:
                                if x==ItemNumber:
                                        pad.addstr(x,0,"..",curses.color_pair(1))
                                else: pad.addstr(x,0,"..")
         else:
-               for x in range(0,len(directorylist)):
+               for x in range(0,len(DirectoryList)):
                        if x==ItemNumber:
-                               pad.addstr(x,0,directorylist[x],curses.color_pair(1))
-                       else: pad.addstr(x,0,directorylist[x])
-        pad.addstr(40,0,"Press Ctrl+x for exit")
-        pad.refresh( 0,0, 5,5, 49,99)
+                               pad.addstr(x,0,DirectoryList[x],curses.color_pair(1))
+                       else: pad.addstr(x,0,DirectoryList[x])
+        pad.addstr(15,0,"Press Ctrl+x for exit")
+        pad.refresh( 0,0, 2,5, 19,99)
         
 def UpperItem(ItemNumber):
-        directorylist=os.listdir()
+        DirectoryList=os.listdir()
         if len(os.getcwd())>3:
-                if ItemNumber<0: ItemNumber=(len(directorylist))
+                if ItemNumber<0: ItemNumber=(len(DirectoryList))
         else:
-                if ItemNumber<0: ItemNumber=(len(directorylist)-1)
+                if ItemNumber<0: ItemNumber=(len(DirectoryList)-1)
         print(ItemNumber)
-        pad = curses.newpad(100, 50)
-        curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
-        if len(os.getcwd())>3: 
-               for x in range(0,len(directorylist)+1):
-                       if x>0:
-                               if x==ItemNumber:
-                                       pad.addstr(x,0,directorylist[x-1],curses.color_pair(1))
-                               else: pad.addstr(x,0,directorylist[x-1])
-                       else:
-                               if x==ItemNumber:
-                                       pad.addstr(x,0,"..",curses.color_pair(1))
-                               else: pad.addstr(x,0,"..")
-        else:
-               for x in range(0,len(directorylist)):
-                       if x==ItemNumber:
-                               pad.addstr(x,0,directorylist[x],curses.color_pair(1))
-                       else: pad.addstr(x,0,directorylist[x])
-        pad.addstr(40,0,"Press Ctrl+x for exit")
-        pad.refresh( 0,0, 5,5, 49,99)
+        ShowWhatDirectoryContains(DirectoryList,ItemNumber)
         return ItemNumber
 
 def LowerItem(ItemNumber):
-       directorylist=os.listdir()
+       DirectoryList=os.listdir()
        if len(os.getcwd())>3:
-               if ItemNumber>(len(directorylist)): ItemNumber=0
+               if ItemNumber>(len(DirectoryList)): ItemNumber=0
        else:
-               if ItemNumber>(len(directorylist)-1): ItemNumber=0
+               if ItemNumber>(len(DirectoryList)-1): ItemNumber=0
        print(ItemNumber)
-       pad = curses.newpad(100, 50)
-       curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
-       if len(os.getcwd())>3: 
-               for x in range(0,len(directorylist)+1):
-                       if x>0:
-                               if x==ItemNumber:
-                                       pad.addstr(x,0,directorylist[x-1],curses.color_pair(1))
-                               else: pad.addstr(x,0,directorylist[x-1])
-                       else:
-                               if x==ItemNumber:
-                                       pad.addstr(x,0,"..",curses.color_pair(1))
-                               else: pad.addstr(x,0,"..")
-       else:
-               for x in range(0,len(directorylist)):
-                       if x==ItemNumber:
-                               pad.addstr(x,0,directorylist[x],curses.color_pair(1))
-                       else: pad.addstr(x,0,directorylist[x])
-       pad.addstr(40,0,"Press Ctrl+x for exit")
-       pad.refresh( 0,0, 5,5, 49,99)
+       ShowWhatDirectoryContains(DirectoryList,ItemNumber)
        return ItemNumber
 
 def ChangeDirectory(ItemNumber):
-        directorylist=os.listdir()
-        if ItemNumber==0 and len(os.getcwd())>3: os.chdir("..")
+        DirectoryList=os.listdir()
+        if ItemNumber==0 and len(os.getcwd())>3:
+                os.chdir("..")
+                ItemNumber=0
         else:
-                if "." in directorylist[ItemNumber]:
+                if "." in DirectoryList[ItemNumber-1]:
                         print("It's file")
-                else: os.chdir(directorylist[ItemNumber])
-        ShowWhatDirectoryContains()
+                else:
+                        if len(os.getcwd())>3:
+                                os.chdir(DirectoryList[ItemNumber-1])
+                        else:
+                                os.chdir(DirectoryList[ItemNumber])
+                        ItemNumber=0
+        ShowWhatDirectoryContains(DirectoryList,ItemNumber)
+        return ItemNumber
 
-os.system("mode con cols=100 lines=50")
+os.system("mode con cols=100 lines=20")
 stdscr=curses.initscr()
 ItemNumber=0
 curses.start_color()
@@ -95,7 +67,8 @@ stdscr.refresh()
 curses.noecho()
 curses.cbreak()
 stdscr.keypad(True)
-ShowWhatDirectoryContains()
+DirectoryList=os.listdir()
+ShowWhatDirectoryContains(DirectoryList,ItemNumber)
 
 while True:
     key=stdscr.getch()
@@ -107,7 +80,7 @@ while True:
             ItemNumber=ItemNumber-1
             ItemNumber=UpperItem(ItemNumber)
     elif key==10:
-            ChangeDirectory(ItemNumber)  
+            ItemNumber=ChangeDirectory(ItemNumber)  
     else: print(key)
 
 curses.nocbreak()
