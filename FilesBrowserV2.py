625d99f2be38e416fe1e6132b81aa23717dced44
yami5 <yami5@wp.pl>
2017-08-03 09:02:51 +0200

    Scrolling is implemented

--- FilesBrowserV2.py
+++ FilesBrowserV2.py
@@ -11,31 +11,49 @@ def SetDirectoryList():
     DirectoryList=list(DirectoryList)
     return DirectoryList
 
-def SetLenght(DirectoryList):
-    if len(DirectoryList)>15: return 15
+def SetLenght(DirectoryList,Shift):
+    if len(DirectoryList)>15: return 15+Shift
     elif len(DirectoryList)<=15: return len(DirectoryList)
     else: print("you fucked up")
 
 def ShowWhatDirectoryContains(DirectoryList,Shift):
     pad = curses.newpad(20, 40)
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_RED)
-    Lenght=SetLenght(DirectoryList)
-    for x in range(0,Lenght):
-        if DirectoryList[x+Shift]=="..":
-            pad.addstr(x, 0, DirectoryList[x+Shift])
-        if DirectoryList[x+Shift]!=".." and len(DirectoryList[x])>24:
-            pad.addstr(x, 0, DirectoryList[x+Shift][0:20]+"...")
-        if DirectoryList[x+Shift]!=".." and len(DirectoryList[x])<=24:
-            pad.addstr(x, 0, DirectoryList[x+Shift])
+    Lenght=SetLenght(DirectoryList,Shift)
+    for x in range(0+Shift,Lenght):
+        if DirectoryList[x]=="..":
+            pad.addstr(x-Shift, 0, DirectoryList[x])
+        if DirectoryList[x]!=".." and len(DirectoryList[x])>24:
+            pad.addstr(x-Shift, 0, DirectoryList[x][0:20]+"...")
+        if DirectoryList[x]!=".." and len(DirectoryList[x])<=24:
+            pad.addstr(x-Shift, 0, DirectoryList[x])
         else: print("you fucked up")
     pad.addstr(18,0,"Press Ctrl+x for exit")
-    pad.refresh(0, 0, 0, 0, 19, 39)
+    pad.refresh(0, 0, 1, 0, 19, 39)
+
+def ScrollListUp(DirectoryList,ItemNumber,Shift):
+    Shift-=15
+    if Shift+0<0:
+        Shift=0
+    else: ItemNumber=0
+    print("Shift=",Shift)
+    ShowWhatDirectoryContains(DirectoryList,Shift)
+    return [Shift, ItemNumber]
+
+def ScrollListDown(DirectoryList,ItemNumber,Shift):
+    Shift+=15
+    if Shift+len(DirectoryList)>len(DirectoryList):
+        Shift=len(DirectoryList)-Shift
+        print("here")
+    else: ItemNumber=0
+    print("Shift=",Shift)
+    ShowWhatDirectoryContains(DirectoryList,Shift)
+    return [Shift, ItemNumber]
 
 os.system("mode con cols=40 lines=20")
 stdscr=curses.initscr()
-ItemNumber=0
-Lenght=14
 Shift=0
+ItemNumber=0
 curses.start_color()
 stdscr.refresh()
 curses.noecho()
@@ -48,6 +66,7 @@ for disk in c.Win32_LogicalDisk(DriveType=3):
 ColumnNumber=0
 DirectoryList=SetDirectoryList()
 ShowWhatDirectoryContains(DirectoryList,Shift)
+print("len=",len(DirectoryList))
 
 while True:
     key=stdscr.getch()
@@ -72,12 +91,13 @@ while True:
     # #Enter
     # elif key==10:
     #     ColumnNumber,ItemNumber,Shift=ChangeDirectory(ItemNumber,PartitionList,ColumnNumber,Lenght,Shift)
-    # # Page Up 339
-    # elif key == 339:
-    #     Shift, ItemNumber, Lenght=ScrollListUp(DirectoryList,ItemNumber,PartitionList,ColumnNumber,Lenght,Shift)
-    # # Page Down 338
-    # elif key == 338:
-    #     Shift, ItemNumber, Lenght=ScrollListDown(DirectoryList,ItemNumber,PartitionList,ColumnNumber,Lenght,Shift)
+    # Page Up 339
+    elif key == 339:
+        Shift, ItemNumber=ScrollListUp(DirectoryList,ItemNumber,Shift)
+    # Page Down 338
+    elif key == 338:
+        Shift, ItemNumber=ScrollListDown(DirectoryList,ItemNumber,Shift)
+    else: print(key)
 
 else: print(key)
 curses.nocbreak()
